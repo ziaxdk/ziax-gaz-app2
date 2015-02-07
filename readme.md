0129f75aa4a4a38bb3dd995942146f8006e04888
ziaxdk <ziaxdk@gmail>
2015-02-07 21:31:56 +0100

    ..

---
+++ readme.md
@@ -0,0 +1,14 @@
+# What to do
+
+## Desktop
+
+```bash
+$ gulp dev
+```
+
+## Device
+
+```bash
+$ cd src
+$ phonegap serve
+```
--- src/www/js/app.js
+++ src/www/js/app.js
@@ -43,33 +43,7 @@ angular.module('starter', ['ionic', 'ngCordova'])
     console.log('run complete');
   });
 })
-.controller('MyController', function($scope, $ionicPopover) {
-
-  // .fromTemplateUrl() method
-  $ionicPopover.fromTemplateUrl('my-popover.html', {
-    scope: $scope,
-  }).then(function(popover) {
-    $scope.popover = popover;
-  });
+.controller('RootController', function($scope, $ionicPopover) {
 
 
-  $scope.openPopover = function($event) {
-    alert('open');
-    $scope.popover.show($event);
-  };
-  $scope.closePopover = function() {
-    $scope.popover.hide();
-  };
-  //Cleanup the popover when we're done with it!
-  $scope.$on('$destroy', function() {
-    $scope.popover.remove();
-  });
-  // Execute action on hide popover
-  $scope.$on('popover.hidden', function() {
-    // Execute action
-  });
-  // Execute action on remove popover
-  $scope.$on('popover.removed', function() {
-    // Execute action
-  });
 });
